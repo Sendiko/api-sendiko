91deec15409d08becc34243de26b375e84379215
Nashir Resta <nashirresta7@gmail.com>
2025-07-27 11:46:39 +0700

    fix: add allowed cors origin

--- src/lib/constant/allowedCors.ts
+++ src/lib/constant/allowedCors.ts
@@ -1,1 +1,6 @@
-export const allowedCors: string[] = ['http://localhost:3000']
+export const allowedCors: string[] = [
+  'http://localhost:3000',
+  'http://localhost:5173',
+  'https://portofolio-sendiko.vercel.app',
+  'https://www.sendiko.my.id',
+]
